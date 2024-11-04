b7fe0925c6837f4a6ceddd6639175a786a13c5f6
VietDucFCB <116992108+VietDucFCB@users.noreply.github.com>
2024-11-04 19:56:40 +0700

    .

--- TH_W2_AI/main.py
+++ TH_W2_AI/main.py
@@ -20,6 +20,7 @@ def main():
 
         output_file_name = f"{solve_method}"
         # Draw legend if legend_flag is set
+
         if legend_flag:
             if legend_flag[0].upper() == 'T':
                 output_file_name += "_legend.png"
