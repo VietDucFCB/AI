c67eebc8c49a699dae2b99cdfdcd529efdcf82f6
VietDucFCB <116992108+VietDucFCB@users.noreply.github.com>
2024-11-09 20:35:31 +0700

    .

--- TH_AI_W3/code.py
+++ TH_AI_W3/code.py
@@ -199,7 +199,6 @@ if __name__ == "__main__":
 
         if inputCode1 == 0 or inputCode2 == 0:
             break
-
         startCity = citiesCode[inputCode1]
         endCity = citiesCode[inputCode2]
 
