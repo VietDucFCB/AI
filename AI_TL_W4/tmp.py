85eae838d3a29820b332d375ccd8d5522a54c030
VietDucFCB <116992108+VietDucFCB@users.noreply.github.com>
2024-12-23 23:42:13 +0700

    .

--- AI_TL_W4/tmp.py
+++ AI_TL_W4/tmp.py
@@ -368,7 +368,6 @@ def print_result(result_node):
     print("Đường đi tìm được:", *print_res, sep=' -> ')
     return result
 # Hàm hiển thị kết quả
-
 def visualize(graph, poly_list, start, goal, result, title, ax):
     ax.set_title(title)
     ax.plot([start.x], [start.y], 'ro', label="Start")
